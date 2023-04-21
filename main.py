236beb28c48c44713d64422b984f4be3f6c209ff
Kjell Le <kjell.le@skole.rogfk.no>
2023-04-21 11:01:36 +0200

    game function done, logic not

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ screen = pygame.display.set_mode((winsizeW, winsizeH))
 clock = pygame.time.Clock()
 running = True
 dt = 0
+background = "purple"
 
 def motstand_create_single(posX, posY, width, height, bandW):
     resistorSizeW = width #200
@@ -62,11 +63,21 @@ def motstand_create_single(posX, posY, width, height, bandW):
 
 
 
-def draw_resistor(motstand, c1, c2, c3, c4, bg=(230, 242, 255)):
-    colors = [bg, c1, c2, c3, c4]
+def draw_resistor(motstand, c1, c2, c3, c4, bg=(230, 242, 255), state=0):
+    if state == 0:
+        pygame.draw.rect(screen, "black", motstand[0])
+        return
+    elif state == 1:
+        colors = [bg, c1, c2, c3, c4]
 
-    for c, m in zip(colors, motstand):
-        pygame.draw.rect(screen, c, m)
+        for c, m in zip(colors, motstand):
+            pygame.draw.rect(screen, c, m)
+    elif state == 2:
+        pass
+        # pygame.draw.rect(screen, background, motstand[0])
+    else:
+        print("Not implemented state")
+
 
 def motstandPoscalc(motstandSize, winSize, X, Y, r, c):
     (motstandW, motstandH) = motstandSize
@@ -88,6 +99,11 @@ def motstand_create(motstandSize, X, Y):
 
     return motstand
 
+def reset_state(states):
+    for i in range(len(states)):
+        if states[i] != 2:
+            states[i] = 0
+
 motstandW = 100
 motstandH = 50
 X = 5
@@ -96,41 +112,78 @@ Y = 4
 motstand_colors = [cg.random_color() for _ in range(X*Y//2)]
 motstand_colors += motstand_colors
 
+motstand = motstand_create((motstandW, motstandH), X, Y)
+
+current_motstand = None
+prev_motstand = None
+
+# states: 0 off
+# states: 1 on
+# states: 2 hidden
+
+states = [0] * X*Y
+num_click = 0
+reset_timer = False
+fps = 60
+time_counter = 0
+
 while running:
+    if reset_timer:
+        time_counter += 1
+        if time_counter >= fps:
+           time_counter = 0 
+           num_click = 0
+           reset_timer = False
+           reset_state(states)
+
     # poll for events
     # pygame.QUIT event means the user clicked X to close your window
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.MOUSEBUTTONUP:
+            pos = pygame.mouse.get_pos()
+            for idx, m in enumerate(motstand):
+                if reset_timer:
+                    break
+
+                if m[0].collidepoint(pos):
+                    prev_motstand = current_motstand
+                    current_motstand = idx
+
+                    state = states[idx]
+                    if state == 0:
+                        states[idx] = 1
+                    elif state == 1:
+                        break
+                        # states[idx] = 0
+                    elif state == 2:
+                        break
+                    else:
+                        raise ("State not implemented", state)
+
+                    print(f"Resistor index: {idx}, state from state to {states[idx]}")
+
+                    num_click += 1
+                    print(f"Number of clicks: {num_click}")
+                    if num_click >= 2:
+                        print("current_motstand:", current_motstand)
+                        print("prev_motstand:", prev_motstand)
+
+                        if (motstand_colors[prev_motstand] == motstand_colors[current_motstand]):
+                            states[prev_motstand] = 2
+                            states[current_motstand] = 2
+
+                        reset_timer = True
+                    # if num_click >= 2:
+                    #     num_click = 0
+                    #     reset_state(states)
 
     # fill the screen with a color to wipe away anything from last frame
-    screen.fill("purple")
+    screen.fill(background)
 
-    # motstandW = 100
-    # posX = (winsizeW - 2*motstandW) / (1 + 2)
-
-    # motstand = motstand_create(posX, 0, motstandW, 50, 5)
-    # motstand1 = motstand_create(posX + motstandW + posX, 0, motstandW, 50, 5)
-
-
-    motstand = motstand_create((motstandW, motstandH), X, Y)
-    # posX = motstandPoscalc(motstandW, winsizeW, N, 0)
-    # motstand = motstand_create(posX, 0, motstandW, 50, 5)
-    #
-    # posX = motstandPoscalc(motstandW, winsizeW, N, 1)
-    # motstand1 = motstand_create(posX, 0, motstandW, 50, 5)
-    #
-    # posX = motstandPoscalc(motstandW, winsizeW, N, 2)
-    # motstand2 = motstand_create(posX, 0, motstandW, 50, 5)
-
-    for m, colors in zip(motstand, motstand_colors):
-        draw_resistor(m, *colors, "violet")
-        # draw_resistor(m, "red", "blue", "green", "yellow", "violet")
-
-    # draw_resistor(motstand1, "red", "blue", "green", "yellow", "violet")
-    # draw_resistor(motstand2, "red", "blue", "green", "yellow", "violet")
-
-    # rgb(230, 242, 255)
+    for m, colors, s in zip(motstand, motstand_colors, states):
+        draw_resistor(m, *colors, (120,120,120), s)
 
     # flip() the display to put your work on screen
     pygame.display.flip()
@@ -138,6 +191,6 @@ while running:
     # limits FPS to 60
     # dt is delta time in seconds since last frame, used for framerate-
     # independent physics.
-    dt = clock.tick(60) / 1000
+    dt = clock.tick(fps) / 1000
 
 pygame.quit()
